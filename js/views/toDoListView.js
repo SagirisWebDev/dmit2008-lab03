0086b540d3771d6405ecb2b968fe30066771f994
Tiegan Benson <tiegben@gmail.com>
2024-03-07 12:28:58 -0700

    add template files to repo

---
+++ js/views/toDoListView.js
@@ -0,0 +1,10 @@
+import { toDoItemTemplate } from "js/templates/toDoItemTemplate";
+
+let view;
+
+const testObj = {
+  uid: 1234,
+  todo: "clean cupboards",
+  category: "chores",
+  status: "pending"
+}
